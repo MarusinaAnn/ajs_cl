01765a1c3ea9e0978c16a7305ea9027779aaca4d
Ann Marusina <an.marusina2010@yandex.ru>
2023-09-20 22:11:22 +0300

    chngs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-function info() {
-    const m = new Map();
-    m.set('name', 'ajs');
-    m.set('version', '1.0.0');
-    return m;
-  }
+// function info() {
+//     const m = new Map();
+//     m.set('name', 'ajs');
+//     m.set('version', '1.0.0');
+//     return m;
+//   }
   
-  export default {
-    info
-  };
+//   export default {
+//     info
+//   };
+
+const ajs = require('@marusinaann/ajs');
+
+console.log(ajs.info());
